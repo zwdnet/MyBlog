a81474a91e8420e6797e4b85aaba021301866aea
赵瑜敏(Zhao Yumin) <zwdnet@163.com>
2018-11-30 15:19:02 +0800

    A new blog articles.

--- make.py
+++ make.py
@@ -10,5 +10,8 @@ elif gpus == "post":
     os.system("hexo clean")
     os.system("hexo generate")
     os.system("hexo deploy")
+elif gpus == "push":  #向github同步本地项目
+    os.system("hexo clean")
+    os.system("")
 else:
     print("输入错误，参数必须为test或者post，如在本地测试用test，如上传并发布用post\n")
