5e4da96a8f9db570161c4d34fbe121e92a6bb6b2
赵瑜敏(Zhao Yumin) <zwdnet@163.com>
2018-11-30 15:21:09 +0800

    修改了make.py，增加了push功能

--- make.py
+++ make.py
@@ -12,6 +12,7 @@ elif gpus == "post":
     os.system("hexo deploy")
 elif gpus == "push":  #向github同步本地项目
     os.system("hexo clean")
-    os.system("")
+    os.system("git add .")
+    os.system("git commit")
 else:
     print("输入错误，参数必须为test或者post，如在本地测试用test，如上传并发布用post\n")
