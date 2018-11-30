91ef12ed8e0f49a115a1eb40c0097b881e9144bd
赵瑜敏(Zhao Yumin) <zwdnet@163.com>
2018-11-30 15:29:34 +0800

    增加了一篇博文

--- make.py
+++ make.py
@@ -13,6 +13,12 @@ elif gpus == "post":
 elif gpus == "push":  #向github同步本地项目
     os.system("hexo clean")
     os.system("git add .")
-    os.system("git commit")
+    message = "增加了一篇博文"
+    command = "git commit -a -m"
+    command += ' "'
+    command += message
+    command += '"'
+    os.system(command)
+    os.system("git push origin master")
 else:
     print("输入错误，参数必须为test或者post，如在本地测试用test，如上传并发布用post\n")
